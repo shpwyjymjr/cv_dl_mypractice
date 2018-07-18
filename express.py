0e149990aac05e7f446c95f894cdda37b595a215
Elvis <95713570@qq.com>
2018-07-18 20:34:12 +0800

    图像的表示

---
+++ express.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cv2
+import matplotlib .pyplot as plt
+
+img = np.array([
+    [[255, 0, 0], [0, 255, 0], [0, 0, 255]],
+    [[255, 255, 0], [255, 0, 255], [0, 255, 255]],
+    [[255, 255, 255], [128, 128, 128], [0, 0, 0]],
+], dtype = np.uint8)
+
+plt.imsave('img_pyplot.jpg', img)
+cv2.imwrite('img_cv2.jpg', img)
